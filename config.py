5f2aa904e5e10ded9a65b65d77201bc917a89677
ajammil <abdurrumanmohamed@gmail.com>
2021-07-10 21:58:14 +0200

    Config file added with enviroment variables.

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+
+class BaseConfig:
+    SERVER_PATH = 'http://127.0.0.1:5000/'
+    MAIL_SERVER = 'smtp.zoho.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    SECRET_KEY = 'decentorageSecretKeyIsSALTS'
+    CODE_KEY = 'decentorageCode'
+    MAIL_USERNAME = 'no-reply@decentorage.tech'
+    MAIL_PASSWORD = ''
+    FRONT_END_ROOT = 'http://www.decentorage.tech'
+
+
+class DevelopmentConfig(BaseConfig):
+    DATABASE_URI =  os.environ['MONGODB_DEV_URI']
+    DATABASE_NAME = os.environ['MONGODB_DEV_NAME']
+    ENV = 'development'
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    ENV = 'production'
+    DEBUG = False
+    DATABASE_URI = os.environ['MONGODB_PROD_URI']
+    DATABASE_NAME = os.environ['MONGODB_PROD_NAME']
