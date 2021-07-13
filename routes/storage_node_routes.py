4691fd890c82d1d0e2923378627cb41ad64a91f0
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2021-07-13 19:17:43 +0200

    Added storage node routes that will be implemented

--- routes/storage_node_routes.py
+++ routes/storage_node_routes.py
@@ -1,11 +1,16 @@
 from controllers import heartbeat, withdraw, signin, signup
 import app
+
 storage_url_prefix = '/storage'
 
 
 def add_storage_urls():
-    app.app.add_url_rule(storage_url_prefix + "/signup", view_func=signup, methods=["POST"])
-    app.app.add_url_rule(storage_url_prefix + "/signin", view_func=signin, methods=["POST"])
+    # Get Methods
     app.app.add_url_rule(storage_url_prefix + "/heartbeat", view_func=heartbeat, methods=["GET"])
     app.app.add_url_rule(storage_url_prefix + "/withdraw", view_func=withdraw, methods=["GET"])
-
+    # Post Methods
+    app.app.add_url_rule(storage_url_prefix + "/signup", view_func=signup, methods=["POST"])
+    app.app.add_url_rule(storage_url_prefix + "/signin", view_func=signin, methods=["POST"])
+    app.app.add_url_rule(storage_url_prefix + "/auditResponse", methods=["POST"])  # result
+    app.app.add_url_rule(storage_url_prefix + "/clientConnectionPort", methods=["POST"])  # port, communication port
+    app.app.add_url_rule(storage_url_prefix + "/publicIpChange", methods=["POST"])  # decentorage port, ip_address
--- routes/user_node_routes.py
+++ routes/user_node_routes.py
@@ -4,7 +4,9 @@ user_url_prefix = '/user'
 
 
 def add_user_urls():
+    # Get Methods
+    app.app.add_url_rule(user_url_prefix + "/getFiles", view_func=get_active_contracts, methods=["GET"])
+    # Post Methods
     app.app.add_url_rule(user_url_prefix + "/signup", view_func=signup, methods=["POST"])
     app.app.add_url_rule(user_url_prefix + "/signin", view_func=signin, methods=["POST"])
-    app.app.add_url_rule(user_url_prefix + "/getFiles", view_func=get_active_contracts, methods=["GET"])
     app.app.add_url_rule(user_url_prefix + "/test", view_func=test, methods=["POST"])
