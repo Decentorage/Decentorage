58f2badde2b0b223fd654d3936ea8415e7bb3573
ajammil <abdurrumanmohamed@gmail.com>
2021-07-14 01:46:08 +0200

    Configuration settings updated

--- utils/configuration.py
+++ utils/configuration.py
@@ -14,4 +14,5 @@ class Configuration:
 	decentorage_epoch = datetime.datetime(2020,1,1)
 	# How often does availability reset
 	resetting_months = 2
-	
+	# Minimum availability to send money
+	minimum_availability = 70
